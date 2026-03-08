afa7daaa85331bfaa705e8f7a24d71dbd7c0eebb
Joy <joysu2008@gmail.com>
2026-03-08 11:50:19 -0400

    hero section

---
+++ website/prototype/src/lib/components/Scroll.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let container: HTMLElement;
+  let contentElement: HTMLElement;
+  let trackHeight: number = 0;
+  let progress: number = 0;
+
+  function updateHeight(): void {
+    if (!contentElement) return;
+    trackHeight = contentElement.scrollHeight + window.innerHeight;
+  }
+  
+  function handleScroll(): void {
+    if (!container) return;
+    const totalScroll = container.offsetHeight - window.innerHeight;
+    if (totalScroll <= 0) {
+      progress = 1;
+      return;
+    }
+    const rect = container.getBoundingClientRect();
+    progress = Math.min(Math.max(-rect.top / totalScroll, 0), 1);
+  }
+
+  onMount(() => {
+    updateHeight();
+
+    const observer = new ResizeObserver(() => {updateHeight(); handleScroll();});
+    observer.observe(contentElement)
+    
+    window.addEventListener('scroll', handleScroll)
+    handleScroll();
+    
+    return () => {
+      observer.disconnect();
+      window.removeEventListener('scroll', handleScroll)
+    }
+  })
+</script>
+
+<div class="scroll-underside" style:opacity={Math.max(1-progress*2, 0)}>
+  <p>scroll to unroll...</p>
+</div>
+<div class="scroll-tracker" bind:this={container} style:height={`${trackHeight}px`}>
+  <div class="scroll-viewport">
+    <div class="unrolled-content" style:clip-path={`inset(0 0 ${100 - progress * 100}% 0)`}>
+      <div class="content-wrapper" bind:this={contentElement}>
+        <slot name="content"/>
+      </div>
+    </div>
+    <div class="scroll-handle" style:top={`calc(${progress*100}%)`} style:transform="translateY(-100%)">
+    </div>
+  </div>
+</div>
+
+
+<style>
+  :root {
+    --scroll-color: #002952;
+    --grid-color: #ffffff80;
+  }
+
+  @font-face {
+    font-family: 'R&C Basic Full Bold';
+    src: url('/fonts/r-c-basic-full.ttf');
+  }
+
+  p {
+    font-family: 'R&C Basic Full Bold', sans-serif;
+  }
+
+  .scroll-underside {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  .scroll-tracker {
+    position: relative;
+    width: 100%;
+  }
+  
+  .scroll-viewport {
+    position: sticky;
+    top: 5vw;
+    margin: 0 5vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .unrolled-content {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--scroll-color);
+    background-image:
+      linear-gradient(var(--grid-color) 1px, transparent 1px),
+      linear-gradient(90deg, var(--grid-color) 1px, transparent 1px);
+    background-size: 2rem 2rem; 
+    border-radius: 1rem / 0.5rem;
+    box-shadow: 
+      inset 0 0 0 1rem var(--scroll-color),
+      inset 0 0 0 calc(1rem + 2px) var(--grid-color);
+  }
+
+  .content-wrapper {
+    width: 100%;
+  }
+
+  .scroll-handle {
+    position: absolute;
+    top: 1rem;
+    height: 3rem;
+    background: linear-gradient(
+      to bottom,
+      color-mix(in srgb, black 20%, var(--scroll-color)) 00%,
+      color-mix(in srgb, #2596be 20%, var(--scroll-color)) 50%,
+      color-mix(in srgb, black 20%, var(--scroll-color)) 100%
+    );
+    width: 100%;
+    z-index: 10;
+    border-radius: 1rem / 0.5rem;
+  }
+</style>
--- website/prototype/src/routes/+page.svelte
+++ website/prototype/src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Scroll from '$lib/components/Scroll.svelte';
+  import prototype_logo from '$lib/assets/prototype_logo.webp'
+</script>
+
+
+
+<main>
+  <Scroll>
+    <div slot="content">
+      <div class="hero-wrapper">
+        <img src={prototype_logo} alt="prototype logo" class="hero-logo"/>
+        <h2>December 19-21, 2025</h2>
+        <h2>@ San Jose</h2>
+        <h2>tagline/description goes here</h2>
+      </div>
+    </div>
+  </Scroll>
+  <p>Once upon a time there was a lovely princess.</p>
+  <p>But she had an enchantment upon her of a fearful sort which could only be broken by love's first kiss. She was locked away in a castle guarded by a terrible fire-breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep in the highest room of the tallest tower for her true love and true love's first kiss.</p>
+  <p>Like that's ever gonna happen. What a load of—</p>
+  <p>Somebody once told me the world is gonna roll me, I ain't the sharpest tool in the shed. She was lookin' kind of dumb with her finger and her thumb in the shape of an "L" on her forehead. The years start comin' and they don't stop comin'. Fed to the rules and I hit the ground runnin'. Didn't make sense not to live for fun. Your brain gets smart but your head gets dumb. So much to do, so much to see, so what's wrong with takin' the backstreets? You'll never know if you don't go. You'll never shine if you don't glow.</p>
+  <p>Hey, now, you're an all-star, get your game on, go play. Hey, now, you're a rock star, get the show on, get paid. And all that glitters is gold; only shootin' stars break the mold.</p>
+  <p>It's a cool place and they say it gets colder. You're bundled up now, but wait till you get older. But the meteor men beg to differ, judging by the hole in the satellite picture. The ice we skate is gettin' pretty thin. The water's getting warm so you might as well swim. My world's on fire, how 'bout yours? That's the way I like it and I'll never get bored.</p>
+  <p>Hey, now, you're an all-star, get your game on, go play. Hey, now, you're a rock star, get the show on, get paid. And all that glitters is gold; only shootin' stars break the mold. Go! Go! Go. Go. Go.</p>
+  <p>Hey, now, you're an all-star, get your game on, go play. Hey, now, you're a rock star, get the show on, get paid. And all that glitters is gold; only shootin' stars break the mold.</p>
+  <p>-Think it's in there? -All right. Let's get it! -Whoa. Hold on. Do you know what that thing can do to you? -Yeah, it'll grind your bones for its bread. -Yes, well, actually, that would be a giant. Now, ogres—they're much worse. They'll make a suit from your freshly peeled skin. -No! -They'll shave your liver. Squeeze the jelly from your eyes! Actually, it's quite good on toast.</p>
+  <p>-Back! Back, beast! Back! I warn ya! -Right. This is the part where you run away. And stay out! "Wanted. Fairy tale creatures." All right. This one's full. -Take it away!</p>
+</main>
+
+<style>
+  @font-face {
+    font-family: 'R&C Basic Full Bold';
+    src: url('/fonts/r-c-basic-full.ttf');
+  }
+
+  .hero-wrapper h2 {
+    font-family: 'R&C Basic Full Bold', sans-serif;
+    line-height: 1;
+    margin: 0;
+  }
+
+  .hero-wrapper {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+  }
+
+  .hero-logo {
+    max-width: 80% !important;
+  }
+</style>
