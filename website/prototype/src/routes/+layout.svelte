bdc11c49ca6606072ba054f46d1ff564e53bd52a
Joy Su <joysu2008@gmail.com>
2026-03-16 17:10:34 -0400

    add title/description

--- website/prototype/src/routes/+layout.svelte
+++ website/prototype/src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Prototype | Hack Club</title>
+	<meta name="Description" content="Explore the projects from Prototype, a 2025 AI hardware hackathon by Hack Club and AMD where teens built ambitious AI-powered projects with robot arms, AI PCs, and over 1,000 lbs of hardware."/>
 </svelte:head>
 
 {@render children()}
